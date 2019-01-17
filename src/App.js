3b4bf0d72c4ff2401a712e626666f629454b2024
weiwenl <weiwenl.justin@gmail.com>
2019-01-18 00:54:59 +0800

    app header, search inputs and btn, results div

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// import components
+import AgeInput from './components/inputs/AgeInput';
+import GenderInput from './components/inputs/GenderInput'
+
+// import svg
+import searchbtn from './search.svg'
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-top">
+          <h1 className="App-header">Diagnosis Search</h1>
+        </div>
+
+        <div className="Search-wrapper">
+          <AgeInput />
+          <GenderInput />
+          <input type='image' src={searchbtn} alt='Search' style={{'height': '2.5em', 'padding':'0 auto'}} />
+        </div>
+
+        <div className="Result-wrapper">
+          {/* <Results /> */}
+        </div>
       </div>
     );
   }
