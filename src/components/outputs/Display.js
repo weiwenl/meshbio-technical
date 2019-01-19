35dce1a6fa787c7f40e421c9be4180ec17590ca6
weiwenl <weiwenl.justin@gmail.com>
2019-01-19 23:46:01 +0800

    display styling

--- src/components/outputs/Display.js
+++ src/components/outputs/Display.js
@@ -4,22 +4,45 @@ const Display = (props) => {
   const displayStyles = {
     'backgroundColor': '#e2f6d5',
     'width': '90vw',
-    'height': '25vh',
+    'height': '24vh',
     'padding': '2vh',
     'border': '2px solid #ccc',
-    'marginBottom': '2em'
+    'marginBottom': '2em',
+    'display': 'flex'
     }
+
+  const identityStyles = {
+    'width': '40vw',
+    'height': '24vh'
+  }
+
+  const comparisonStyles = {
+    'width': '40vw',
+    'height': '24vh',
+    'backgroundColor': '#ccc',
+    'float': 'left',
+    'textAlign': 'center'
+  }
 
   return(
     <div style={displayStyles}>
-      <h4>Subject ID: {props.name}</h4>
-      <h4>Age: {props.age}</h4>
-      <h4>Gender: {props.gender === 1? 'Female' : 'Male'}</h4>
+      <div style={identityStyles}>
+        <h4>Subject ID: {props.name}</h4>
+        <h4>Age: {props.age}</h4>
+        <h4>Gender: {props.gender === 1? 'Female' : 'Male'}</h4>
 
-      <h4>Blood Pressure: {props.bp}</h4>
-      <h4>Body Mass: {props.mass}</h4>
-      <h4>Smoker: {props.smoker === 0? 'No' : 'Yes'}</h4>
-      <h4>Has Diabetes: {props.diabetes === 0? 'No' : 'Yes'}</h4>
+        <h4>Blood Pressure: {props.bp}</h4>
+        <h4>Body Mass: {props.mass}</h4>
+        <h4>Smoker: {props.smoker === 0? 'No' : 'Yes'}</h4>
+        <h4>Has Diabetes: {props.diabetes === 0? 'No' : 'Yes'}</h4>
+      </div>
+      <div style={comparisonStyles}>
+        <h3>Diagnosis</h3>
+        
+      </div>
+
+
+
     </div>
   );
 }
