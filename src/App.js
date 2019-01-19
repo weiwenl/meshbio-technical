fa0693ded331cf4c1d549d68b340a3283233ce85
weiwenl <weiwenl.justin@gmail.com>
2019-01-19 23:17:58 +0800

    uplifting state

--- src/App.js
+++ src/App.js
@@ -3,21 +3,87 @@ import './App.css';
 
 // import components
 import Search from './container/search/Search';
+import Results from './container/results/Results';
+
+// import json data
+import json_data from './dataset.json';
 
 class App extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      firstAge: '',
+      lastAge: '',
+      gender: '',
+      filterData: [],
+      invalidSelection: ''
+    }
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+    this.onClickHandler = this.onClickHandler.bind(this);
+  }
+
+  onChangeHandler(e) {
+    if(e.target.name === 'fage') {
+      this.setState({
+        firstAge: parseInt(e.target.value)
+      });
+    }
+
+    else if(e.target.name === 'lage'){
+      this.setState({
+        lastAge: parseInt(e.target.value)
+      });
+    }
+    else if(e.target.name === 'gender'){
+      this.setState({ //convert from the string to num to match dataset
+        gender: parseInt(e.target.value)
+      });
+    }
+  }
+
+  onClickHandler(e) {
+    if(this.state.firstAge !== null &&
+      this.state.lastAge !== null && (
+      this.state.gender === 1 || this.state.gender === 2)
+      && (this.state.firstAge !== this.state.lastAge)) {
+
+        const searchData = getData(this.state.firstAge, this.state.lastAge,
+          this.state.gender, json_data);
+          console.log(searchData);
+
+          this.setState({
+            invalidSelection: false,
+            filterData: searchData
+          });
+      }
+
+    else {
+      console.log('Invalid selections');
+      this.setState({
+        invalidSelection: true
+      });
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-top">
           <h1 className="App-header">Diagnosis Search</h1>
+          <div className="Search-wrapper">
+            <Search
+              onChange={this.onChangeHandler}
+              onClick={this.onClickHandler}
+            />
+          </div>
         </div>
 
-        <div className="Search-wrapper">
-          <Search />
-        </div>
+
 
         <div className="Result-wrapper">
-          {/* <Results /> */}
+          <Results
+            data={this.state.filterData}
+          />
         </div>
       </div>
     );
@@ -25,3 +91,14 @@ class App extends Component {
 }
 
 export default App;
+
+// filter dataset by age range and gender to get user requested data
+function getData(firstAge, lastAge, gender, data) {
+  let filteredData = data.filter(function (entry) {
+    return ((entry['Age, years [float]'] > firstAge) &&
+            (entry['Age, years [float]'] < lastAge)  &&
+            (entry['Gender (enum)'] === gender) );
+  });
+  // console.log(filteredData);
+  return filteredData;
+}
--- src/container/search/Search.js
+++ src/container/search/Search.js
@@ -6,94 +6,27 @@ import GenderInput from '../../components/inputs/GenderInput';
 
 // import svg and json data
 import searchbtn from './search.svg';
-import json_data from './dataset.json';
+
 
 class Search extends Component {
-  constructor(props) {
-    super()
-    this.state = {
-      firstAge: '',
-      lastAge: '',
-      gender: '',
-      filterData: [],
-      invalidSelection: ''
-    }
-    this.onChangeHandler = this.onChangeHandler.bind(this);
-    this.onClickHandler = this.onClickHandler.bind(this);
-  }
-
-  onChangeHandler(e) {
-    if(e.target.name === 'fage') {
-      this.setState({
-        firstAge: parseInt(e.target.value)
-      });
-    }
-
-    else if(e.target.name === 'lage'){
-      this.setState({
-        lastAge: parseInt(e.target.value)
-      });
-    }
-    else if(e.target.name === 'gender'){
-      this.setState({ //convert from the string to num to match dataset
-        gender: parseInt(e.target.value)
-      });
-    }
-  }
-
-  onClickHandler(e) {
-    if(this.state.firstAge !== null &&
-      this.state.lastAge !== null && (
-      this.state.gender === 1 || this.state.gender === 2)
-      && (this.state.firstAge !== this.state.lastAge)) {
-
-        const searchData = getData(this.state.firstAge, this.state.lastAge,
-          this.state.gender, json_data);
-          console.log(searchData);
-
-          this.setState({
-            invalidSelection: false,
-            filterData: searchData
-          });
-      }
-
-    else {
-      console.log('Invalid selections');
-      this.setState({
-        invalidSelection: true
-      });
-    }
-  }
 
   render() {
     return(
       <Fragment>
         <AgeInput
-          firstAge={this.onChangeHandler}
-          lastAge={this.onChangeHandler}/>
+          firstAge={this.props.onChange}
+          lastAge={this.props.onChange}/>
         <GenderInput
-          gender={this.onChangeHandler}/>
+          gender={this.props.onChange}/>
         <input
           type='image'
           src={searchbtn}
           alt='Search'
           style={{'height': '2.5em', 'padding':'0 auto'}}
-          onClick={this.onClickHandler}/>
+          onClick={this.props.onClick}/>
       </Fragment>
     );
   }
 }
 
 export default Search
-
-
-// filter dataset by age range and gender to get user requested data
-function getData(firstAge, lastAge, gender, data) {
-  let filteredData = data.filter(function (entry) {
-    return ((entry['Age, years [float]'] > firstAge) &&
-            (entry['Age, years [float]'] < lastAge)  &&
-            (entry['Gender (enum)'] === gender) );
-  });
-  console.log(filteredData);
-  return filteredData;
-}
